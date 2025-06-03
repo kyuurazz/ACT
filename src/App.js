92b9758be91f11d8ba3e4e5c3459b296c5fae87f
Bilal AlHafidz <91085882+kyuurazz@users.noreply.github.com>
2025-06-03 22:15:18 +0700

    Update App.js
    Hide scrollbar on desktop

--- src/App.js
+++ src/App.js
@@ -9,25 +9,41 @@ import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="bg-black text-white">
-      <Navbar />
-      <main className="pt-16">
-        <Hero />
-        <section id="about" className="w-full bg-black">
-          <About />
-        </section>
-        <section id="join" className="w-full bg-black">
-          <Join />
-        </section>
-        <section id="kegiatan" className="w-full bg-white pt-7">
-          <Kegiatan />
-        </section>
-        <section id="faq" className="w-full bg-black pt-7">
-          <FAQ />
-        </section>
-      </main>
-      <Footer />
-    </div>
+    <>
+      <div className="bg-black text-white">
+        <Navbar />
+        <main className="pt-16">
+          <Hero />
+          <section id="about" className="w-full bg-black">
+            <About />
+          </section>
+          <section id="join" className="w-full bg-black">
+            <Join />
+          </section>
+          <section id="kegiatan" className="w-full bg-white pt-7">
+            <Kegiatan />
+          </section>
+          <section id="faq" className="w-full bg-black pt-7">
+            <FAQ />
+          </section>
+        </main>
+        <Footer />
+      </div>
+
+      <style jsx global>{`
+        @media (min-width: 768px) {
+          /* Untuk browser WebKit (Chrome, Safari) */
+          ::-webkit-scrollbar {
+            width: 0px;
+            background: transparent;
+          }
+          /* Untuk Firefox */
+          html {
+            scrollbar-width: none;
+          }
+        }
+      `}</style>
+    </>
   );
 }
 
